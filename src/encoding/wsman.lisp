80a6043ce924e19cf49b2fa0bc006cbc374314aa
Frank James <frank.a.james@gmail.com>
2014-11-19 22:00:24 +0000

    adding to wsman encoding

--- src/encoding/wsman.lisp
+++ src/encoding/wsman.lisp
@@ -4,14 +4,42 @@
 
 ;; read this document for more information
 ;; http://www.dmtf.org/sites/default/files/standards/documents/DSP0227_1.2.0.pdf
+;; http://www.w3.org/Submission/WS-Transfer/
+
 
 
 (defconstant* +soap-envelope+ "http://www.w3.org/2003/05/soap-envelope")
 (defconstant* +soap-addressing+ "http://schemas.xmlsoap.org/ws/2004/08/addressing")
 (defconstant* +soap-wsman+ "http://schemas.dmtf.org/wbem/wsman/1/wsman.xsd")
+(defconstant* +soap-event+ "http://schemas.xmlsoap.org/ws/2004/09/eventing")
 
+;; set some cl-who options
 (setf cl-who:*downcase-tokens-p* nil)
 (setf cl-who:*attribute-quote-char* #\")
+
+
+
+(defconstant* +wsman-enumerate+ "http://schemas.xmlsoap.org/ws/2004/09/enumeration/Enumerate")
+(defconstant* +wsman-enumerate-response+ "http://schemas.xmlsoap.org/ws/2004/09/enumeration/EnumerateResponse")
+
+(defconstant* +wsman-get+ "http://schemas.xmlsoap.org/ws/2004/09/transfer/Get")
+(defconstant* +wsman-get-response+ "http://schemas.xmlsoap.org/ws/2004/09/transfer/GetResponse")
+
+(defconstant* +wsman-subscribe+ "http://schemas.xmlsoap.org/ws/2004/08/eventing/Subscribe")
+
+(defconstant* +wsman-pull-response+ "http://schemas.xmlsoap.org/ws/2004/09/enumeration/PullResponse")
+
+(defconstant* +wsman-put+ "http://schemas.xmlsoap.org/ws/2004/09/transfer/Put")
+(defconstant* +wsman-put-response+ "http://schemas.xmlsoap.org/ws/2004/09/transfer/PutResponse")
+
+(defconstant* +wsman-delete+ "http://schemas.xmlsoap.org/ws/2004/09/transfer/Delete")
+(defconstant* +wsman-delete-response+ "http://schemas.xmlsoap.org/ws/2004/09/transfer/DeleteResponse")
+
+(defconstant* +wsman-create+ "http://schemas.xmlsoap.org/ws/2004/09/transfer/Create")
+(defconstant* +wsman-create-response+ "http://schemas.xmlsoap.org/ws/2004/09/transfer/CreateResponse")
+
+
+
 
 (defun encode-logical-disk ()
   "Basic exmaple of how we might go about encoding the messages. Uses CL-WHO."
@@ -46,21 +74,29 @@
 			
 (defparameter *wsman-stream* *standard-output*)
 
-(defun encode-wsman-envelope (header body)
-  (cl-who:with-html-output (*wsman-stream*)
-    (:|s:Envelope| :|xmlns:s| +soap-envelope+
-	               :|xmlns:a| +soap-addressing+
-				   :|xmlns:w| +soap-wsman+
-      (encode-wsman-header header)
-      (encode-wsman-body body))))
+(defun wformat (format-string &rest args)
+  (apply #'format *wsman-stream* format-string args))
 
-(defun encode-wsman-header (content)
-  (cl-who:with-html-output (*wsman-stream*)
-    (:|s:Header| content)))
+(defun encode-wsman-envelope (header body)
+  (wformat "<s:Envelope xmlns:s=\"~A\" xmlns:a=\"~A\" xmlns:w=\"~A\">"
+		   +soap-envelope+ +soap-addressing+ +soap-wsman+)
+  (encode-wsman-header header)
+  (encode-wsman-body body)
+  (wformat "</s:Envelope>"))
+
+(defun encode-wsman-header (list)
+  (wformat "<s:Header>")
+  (dolist (h list)
+	(destructuring-bind (name attributes content) h
+	  (wformat "<~A" name)
+	  (dolist (a attributes)
+		(destructuring-bind (aname aval) a
+		  (wformat " ~A=~A" aname aval)))
+	  (wformat ">")
+	  (wformat "~A" content)
+	  (wformat "</~A>" name)))
+  (wformat "</s:Header>"))
 
 (defun encode-wsman-body (body)
-  (cl-who:with-html-output (*wsman-stream*)
-    (:|s:Body| body)))
+  (wformat "<s:Body>~A</s:Body>" body))
 
-
-  
