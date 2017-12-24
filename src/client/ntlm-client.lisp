360b673ee832ebdff6ec06b7a6eea34211b9107f
Frank James <frank.a.james@gmail.com>
2017-12-24 07:58:56 +0000

    fix compilation errors

--- src/client/ntlm-client.lisp
+++ src/client/ntlm-client.lisp
@@ -26,36 +26,36 @@
 (defun generate-reply (challenge username domain computer-name password-md4 version)
   "Generate an AUTHENTICATE message from the challenge"
   (when (stringp password-md4)
-    (setf password-md4 (ntlm:password-md4 password-md4)))
+    (setf password-md4 (ntlm::password-md4 password-md4)))
                  
-  (let* ((lmowf (ntlm:lmowf-v2 username domain password-md4))
-         (ntowf (ntlm:ntowf-v2 username domain password-md4))
+  (let* ((lmowf (ntlm::lmowf-v2 username domain password-md4))
+         (ntowf (ntlm::ntowf-v2 username domain password-md4))
          (server-challenge (cdr (assoc :server-challenge challenge)))
-         (client-challenge (ntlm:client-challenge))
+         (client-challenge (ntlm::client-challenge))
          (time (cdr (assoc :timestamp (cdr (assoc :target-info challenge)))))
          (target-info-buffer (cdr (assoc :target-info-buffer challenge)))
-         (temp (ntlm:make-temp time client-challenge target-info-buffer))
-         (lm-response (ntlm:lm-response-v2 lmowf server-challenge client-challenge))
-         (nt-response (ntlm:nt-response-v2 ntowf
+         (temp (ntlm::make-temp time client-challenge target-info-buffer))
+         (lm-response (ntlm::lm-response-v2 lmowf server-challenge client-challenge))
+         (nt-response (ntlm::nt-response-v2 ntowf
                                            server-challenge
                                            temp))
-         (session-base-key (ntlm:session-base-key-v2 ntowf 
+         (session-base-key (ntlm::session-base-key-v2 ntowf 
                                                      server-challenge
                                                      temp))
-         (key-exchange-key (ntlm:key-exchange-key session-base-key
+         (key-exchange-key (ntlm::key-exchange-key session-base-key
                                                   lm-response
                                                   server-challenge
                                                   lmowf))
-         (exported-session-key (ntlm:exported-session-key :negotiate-key-exch t
+         (exported-session-key (ntlm::exported-session-key :negotiate-key-exch t
                                                           :key-exchange-key key-exchange-key)))
-    (ntlm:pack-authenticate-message (cdr (assoc :flags challenge))
+    (ntlm::pack-authenticate-message (cdr (assoc :flags challenge))
                                     :lm-response lm-response
                                     :nt-response nt-response
                                     :domain domain
                                     :username username 
                                     :workstation computer-name
                                     :encrypted-session-key
-                                    (ntlm:encrypted-session-key key-exchange-key exported-session-key)
+                                    (ntlm::encrypted-session-key key-exchange-key exported-session-key)
                                     :version version)))
 
 (defun ntlm-http-request (uri keyword-args &key username domain password-md4 workstation version)
@@ -68,7 +68,7 @@
              :additional-headers 
              `((:authorization . ,(authorization-header 
                                    (let ((msg 
-                                          (ntlm:pack-negotiate-message '(:NEGOTIATE-UNICODE
+                                          (ntlm::pack-negotiate-message '(:NEGOTIATE-UNICODE
                                                                          :NEGOTIATE-OEM :REQUEST-TARGET
                                                                          :NEGOTIATE-NTLM
                                                                          :NEGOTIATE-OEM-DOMAIN-SUPPLIED
@@ -96,7 +96,7 @@
                  :stream stream
                  :additional-headers 
                  `((:authorization . ,(authorization-header
-                                       (generate-reply (ntlm:unpack-challenge-message msg) 
+                                       (generate-reply (ntlm::unpack-challenge-message msg) 
                                                        username domain workstation password-md4 version))))
                  keyword-args)
           (error "No CHALLENGE response")))))
@@ -127,12 +127,101 @@ Ensure it accepts connections: winrm set winrm/config/service @{AllowUnencrypted
 			    :username username 
 			    :domain domain
 			    :password-md4 password
-			    :version (ntlm:make-ntlm-version 1 1 1))
+			    :version (ntlm::make-ntlm-version 1 1 1))
     (declare (ignore ruri stream must-close headers))
 ;;    (format t "HEADERS: ~S~%" headers)
     (if (= status-code 200)
 	(decode-wsman response)
 	(error "Failed ~S: ~S" status-code reason))))
+
+
+(defun call-wsman% (password content &key (username "administrator") (url "http://localhost:5985/wsman") domain)
+  (ntlm:logon-user username password domain)
+  (let ((creds (gss:acquire-credentials :ntlm nil)))
+    (multiple-value-bind (context buffer) (gss:initialize-security-context creds)
+      ;; start by sending a regular request 
+      (multiple-value-bind (response status-code headers ruri stream must-close reason)
+          (drakma:http-request url
+                               :additional-headers 
+                               `((:authorization . ,(format nil 
+                                                            "NTLM ~A" 
+                                                            (cl-base64:usb8-array-to-base64-string buffer))))
+                               :keep-alive t 
+                               :close nil)
+        (declare (ignore ruri must-close))
+        (case status-code
+          (200 (decode-wsman response))
+          (401 
+	   ;; extract the WWW-AUTHENTICATE header
+	   (let ((www (cdr (assoc :www-authenticate headers))))
+	     (unless www (error "No WWW-AUTHENTICATE header"))
+	     ;; get the buffer from the base64 encoded string 
+	     (let ((matches (nth-value 1 (cl-ppcre:scan-to-strings "NTLM ([\\w=\\+/]+)" www))))
+	       (unless matches (error "Not an NTLM authenticate message"))
+	       (multiple-value-bind (context buffer)                        
+		   (gss:initialize-security-context context
+						    :buffer 
+						    (cl-base64:base64-string-to-usb8-array (elt matches 0)))
+		 (declare (ignore context))
+		 (multiple-value-bind (response status-code headers ruri stream must-close reason)
+		     (drakma:http-request url
+					  :additional-headers 
+					  `((:authorization . ,(format nil "NTLM ~A"
+								       (cl-base64:usb8-array-to-base64-string buffer))))
+					  :stream stream
+					  :method :post 
+					  :content content
+					  :content-type "application/soap+xml; charset=UTF-8")
+		   (declare (ignore must-close ruri headers response))
+		   (case status-code 
+		     (200 (decode-wsman stream))
+		     (otherwise (error "FAILED ~A ~A~%" status-code reason))))))))
+          (otherwise (error "FAILED: ~A~%" reason)))))))
+
+
+(defun spnego-http-request (principal &optional (url "http://localhost:2001/"))
+  (let ((creds (gss:acquire-credentials :spnego principal)))
+    (multiple-value-bind (init-context buffer) (gss:initialize-security-context creds)
+      (multiple-value-bind (content status-code headers ruri stream must-close reason)
+          (drakma:http-request url
+                               :additional-headers 
+                               `((:authorization . ,(format nil 
+                                                            "Negotiate ~A" 
+                                                            (cl-base64:usb8-array-to-base64-string buffer))))
+                               :keep-alive t 
+                               :close nil)
+        (declare (ignore ruri must-close))
+        (case status-code
+          (200 content)
+          (401 
+	   ;; extract the WWW-AUTHENTICATE header
+	   (let ((www (cdr (assoc :www-authenticate headers))))
+	     (unless www (error "No WWW-AUTHENTICATE header"))
+	     ;; get the buffer from the base64 encoded string 
+	     (let ((matches (nth-value 1 (cl-ppcre:scan-to-strings "Negotiate ([\\w=\\+/]+)" www))))
+	       (unless matches (error "Not a Negotiate header"))
+	       (multiple-value-bind (context buffer)                        
+		   (gss:initialize-security-context init-context
+						    :buffer 
+						    (cl-base64:base64-string-to-usb8-array (elt matches 0)))
+		 (declare (ignore context))
+		 (multiple-value-bind (content status-code headers ruri stream must-close reason)
+		     (drakma:http-request url
+					  :additional-headers 
+					  `((:authorization . ,(format nil "Negotiate ~A"
+								       (cl-base64:usb8-array-to-base64-string buffer))))
+					  :stream stream)
+		   (declare (ignore must-close ruri stream headers))
+		   (case status-code 
+		     (200 content)
+		     (otherwise (error "FAILED ~A ~A~%" status-code reason))))))))
+          (otherwise (error "FAILED ~A: ~A~%" status-code reason)))))))
+
+
+
+
+
+
 
 
 (defun call-wsman* (password content &key (username "administrator") (uri "http://localhost:5985/wsman") domain)
@@ -144,7 +233,7 @@ Ensure it accepts connections: winrm set winrm/config/service @{AllowUnencrypted
 		     :username username 
 		     :domain domain
 		     :password-md4 password
-		     :version (ntlm:make-ntlm-version 1 1 1)))
+		     :version (ntlm::make-ntlm-version 1 1 1)))
 
 
 
